16da68e56be017b1fd1b00a6dc9fef5c30c35d71
Sermarajsunadar <sermaraj.sundar.syncedge@gmail.com>
2025-07-11 14:37:12 +0530

    added one element 111 files

--- Luxira/preview.keenthemes.com/keen/demo1/header.php
+++ Luxira/preview.keenthemes.com/keen/demo1/header.php
@@ -5486,7 +5486,7 @@
                         class="btn btn-flex btn-icon btn-active-color-primary w-30px h-30px"
                         id="kt_app_header_menu_toggle">
                         <i
-                            class="ki-duotone ki-element-4 fs-1"><span
+                            class="fa-solid fa-boxes-stacked text-dark-700"><span
                                 class="path1"></span><span
                                 class="path2"></span></i>
                     </div>
